ddc6be67b1ef3f912e9243067497cd608799cd40
dan <jh2310699594@gmail.com>
2019-05-20 00:07:12 +0800

    实现银行家算法并为之使用安全性检查

--- 操作系统实验/python/Bankers_Algorithm.py
+++ 操作系统实验/python/Bankers_Algorithm.py
@@ -3,13 +3,14 @@ class Bankers_Algorithm:
     _count = 0
     _processes = 4  # 进程数
     _types = 5  # 资源种类
+    _SecuredSequence = []  # 安全序列
+    _useProcess = 0
     _allavailable = None  # 五种种资源的数量
     _allocation = None  # 分配矩阵
     _need = None  # 需求矩阵
     _max = None  # 最大需求矩阵
     _available = None  # 可利用资源向量
     _work = None
-    _wordAndAllocation = None
     _finish = None
 
     # 初始化所有数据化
@@ -19,8 +20,7 @@ class Bankers_Algorithm:
         need = [[1, 0, 2, 1, 1], [0, 3, 2, 1, 0], [0, 3, 3, 2, 2], [1, 0, 1, 2, 1]]
         available = [0, 0, 0, 0, 0]
         max = [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
-        work = [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
-        wordAndAllocation = [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
+        work = [0, 0, 0, 0, 0]
         temp = [0, 0, 0, 0, 0]
         for i in range(self._processes):
             for j in range(self._types):
@@ -34,7 +34,6 @@ class Bankers_Algorithm:
         self._max = max
         self._available = available
         self._work = work
-        self._wordAndAllocation = wordAndAllocation
         self._finish = [False, False, False, False]
 
     # 打印所有数据
@@ -44,28 +43,102 @@ class Bankers_Algorithm:
         print('_need' + str(self._need))
         print('_max' + str(self._max))
         print('_available' + str(self._available))
-        print('_work' + str(self._work))
-        print('_wordAndAllocation' + str(self._wordAndAllocation))
+        # print('_work' + str(self._work))
         print('_finish' + str(self._finish))
-
-    # todo 实现银行家算法
-    def _brank(self):
-        return True
-
-    # todo 实现安全性算法
-    def _safe(self):
-        return True
 
     # 外部调用
     def toWork(self):
         # self._printAll()
+        # 是否符合银行家算法的规范
         if not self._brank():
             print('进制执行不符合银行家算法，系统不给予资源')
         else:
+            # 是否符合安全行算法
             if not self._safe():
                 print('进程执行不安全，系统不能给予资源')
             else:
-                print('进程可以执行，其执行顺序是：')
+                print('进程可以执行，其执行顺序是：' + str(self._SecuredSequence))
+
+    # todo 实现银行家算法，符合返回true，反之false
+    def _brank(self):
+        # request = [1, 0, 0, 0, 1]
+        request = []
+        for i in range(self._types):
+            a = int(input("请输入request" + str(i) + "值:  "))
+            request.append(a)
+        # 1）如果Request i[j]≤Need[i, j]，转向步骤（2）；否则所需资源数已超过它所宣布的最大值。
+        if not self._requestSmall(request, self._need[self._useProcess]):
+            return False
+        else:
+            # 2）如果Requesti[j]≤Available[i, j]，转向步骤（3）；否则无足够资源，Pi需等待。
+            if not self._requestSmall(request, self._available):
+                return False
+            else:
+                '''系统试探着把资源分配给进程Pi，并修改下面数据结构中的数值：
+　　　　Available[j]=Available[j]-Requesti[j]；
+　　　　Allocation[i, j]=Allocation[i, j]+Requesti[j]；
+　　　　Need[i, j]=Need[i, j]-Requesti[j]；'''
+                self._available = self._subtraction(self._available, request)
+                self._need[self._useProcess] = self._subtraction(self._need[self._useProcess], request)
+                self._allocation[self._useProcess] = self._add(self._allocation[self._useProcess], request)
+                print('修改可利用资源向量为：' + str(self._available))
+                print('修改需求矩阵为：' + str(self._need[self._useProcess]))
+                print('修改分配矩阵为：' + str(self._allocation[self._useProcess]))
+            return True
+
+    # todo 实现安全性算法,安全返回true，反之false
+    def _safe(self):
+        count = 0
+        while count < self._processes:
+            flag = False  # 一次循环中找不到安全的状态，直接变为不安全，结束循环
+            for i in range(self._processes):
+                if not self._finish[i]:  # 只会去从没有执行过的地方执行
+                    if count == 0:  # 开始的时候Work=Available
+                        if self._requestSmall(self._need[i], self._available):
+                            # print(self._need[i])
+                            self._finish[i] = True  # 告诉程序此进程可以通过安全算法
+                            self._work = self._add(self._available,
+                                                   self._allocation[i])  # Work[j]=Work[j]+Allocation[i, j]；
+                            count += 1
+                            # print(self._work)
+                            # print(i)
+                            flag = True
+                            self._SecuredSequence.append(i)  # 记录安全序列的值
+                    else:
+                        if self._requestSmall(self._need[i], self._work):
+                            self._finish[i] = True  # 告诉程序此进程可以通过安全算法
+                            self._work = self._add(self._work,  # debug
+                                                   self._allocation[i])  # Work[j]=Work[j]+Allocation[i, j]；
+                            count += 1
+                            flag = True
+                            # print(self._work)
+                            # print(i)
+                            self._SecuredSequence.append(i)  # 记录安全序列的值
+            if not flag:
+                return False
+        return True
+
+        # 比较两个列表每个的大小，第一个列表值都小于第二个列表的值返回ture，反之返回flase
+
+    def _requestSmall(self, request, others):
+        for i in range(self._types):
+            if request[i] > others[i]:
+                return False  # request 列表的元素有大于第二个的，直接结束
+        return True
+
+    # 返回两个列表每个元素相减的值
+    def _subtraction(self, list1, list2):
+        mylist = []
+        for i in range(self._types):
+            mylist.append(list1[i] - list2[i])
+        return mylist
+
+    # 返回两个列表每个元素相加的值
+    def _add(self, list1, list2):
+        mylist = []
+        for i in range(self._types):
+            mylist.append(list1[i] + list2[i])
+        return mylist
 
 
 demo = Bankers_Algorithm()
